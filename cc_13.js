5b18ac4a9b1107f3e32e740973c6a0ff8a86a868
Fernando <mhfernando778@gmail.com>
2025-03-08 19:13:35 -0500

    Task 2 - added comments

--- cc_13.js
+++ cc_13.js
@@ -1,32 +1,33 @@
-// Task 2 - Adding Employee Cards Dynamically
+// Task 2 - Adding Employee Cards Dynamically //
 
-function addEmployeeCard(name, position) {
+function addEmployeeCard(name, position) { // building an employee card
     const employeeContainer = document.getElementById("employeeContainer");
     const card = document.createElement("div");
     
-    card.setAttribute("class", "employee-card");
+    card.setAttribute("class", "employee-card"); // setting up certain attributes
     card.classList.add("employee-card");
 
-    const employeeName = document.createElement("h2");
+    const employeeName = document.createElement("h2"); // creating a heading for employee name
     employeeName.textContent = name;
 
-    const employeePosition = document.createElement("p");
+    const employeePosition = document.createElement("p"); // Creating a paragraph for employee position
     employeePosition.textContent = position;
 
-    const removeButton = document.createElement("button");
+    const removeButton = document.createElement("button"); // creating a button to remove a card
     removeButton.textContent = "Remove";
     removeButton.setAttribute("class", "remove-button");
     removeButton.addEventListener("click", () => {
         employeeContainer.removeChild(card);
     });
 
-    card.appendChild(employeeName);
-    card.appendChild(employeePosition);
-    card.appendChild(removeButton);
+    card.appendChild(employeeName); // appending 
+    card.appendChild(employeePosition); // appending
+    card.appendChild(removeButton); // appending
 
-    employeeContainer.appendChild(card);
+    employeeContainer.appendChild(card); // appending employee card
 };
 
+// Test cases //
 addEmployeeCard("John Doe", "Teacher");
 addEmployeeCard("Jane Cassidy", "Assistant");
 addEmployeeCard("Janet Jackson", "Singer");
